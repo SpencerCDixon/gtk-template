aac0bdf52ea05bac34f7ffcb78d0016be36a9f35
Spencer Dixon <spencercdixon@gmail.com>
2022-01-28 13:12:30 -0500

    Update template to render a Tuple playground window

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,4 +4,10 @@ set -exuo pipefail
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "${SCRIPT_PATH}/.."
 
-sudo apt install build-essential ninja-build cmake libgtk-3-dev libgtkmm-3.0-dev 
+sudo apt install -y build-essential \
+  ninja-build \
+  cmake \
+  libgtk-3-dev \
+  libgtkmm-3.0-dev \
+  libgtkmm-3.0-doc \
+  devhelp
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <gtkmm.h>
+
+class Window final : public Gtk::Window {
+public:
+    Window() {
+      set_title("Tuple Playground");
+      set_size_request(400, 400);
+      set_default_size(400, 400);
+      show_all();
+    }
+    ~Window() { }
+};
 
 int main(int argc, char **argv)
 {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+    auto app = Gtk::Application::create(argc, argv, "app.tuple.app.playground");
+    Window window;
+    return app->run(window);
 }
